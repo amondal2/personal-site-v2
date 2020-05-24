ea194e11e0e8c1cdbbb94e99aa5e73795ecb8796
amondal <amondal13@gmail.com>
2020-05-24 11:28:40 -0400

    add blog and contact components

---
+++ src/components/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <div className="blogWrapper">
+      <h2 id="blog">Blog</h2>
+      <p>Coming soon!</p>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/components/Contact.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="contactWrapper">
+      <h2 id="contact">Contact</h2>
+      <p>
+        Please drop a line if you'd like to chat! I'm available on these
+        platforms:
+      </p>
+      <ol>
+        <li>
+          <a href="mailto:amondal13@gmail.com" target="_blank" rel="noreferrer">
+            Email
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/amondal2"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://linkedin.com/in/agastyamondal"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://instagram.com/agastya.mondal"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+      </ol>
+    </div>
+  );
+};
+
+export default Contact;
